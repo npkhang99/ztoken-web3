b70222f734f275ff8bdeac186be8df565a863e4c
Khang Nguyen <npkhang99@gmail.com>
2023-10-18 23:43:26 +0700

    Add Approve and Play VRFGame

--- events.js
+++ events.js
@@ -4,19 +4,67 @@ const web3 = new Web3(ethereum)
 let initialized = false
 
 const FAUCET_ADDRESS = '0x21A2970671E05579B5A17C01296c53955290296B'
-const ZTOKEN_ADDRESS = '0xc9ee4a2a5b374ea8e4b86d91a4154258d830b591'
+const TOKENZ_ADDRESS = '0xc9ee4a2a5b374ea8e4b86d91a4154258d830b591'
+const VRFGAME_ADDRESS = '0xe7C610a5Ca34142B22e22616DA96B9b3dd8De916'
+
+const ONE_MWEI = web3.utils.toWei(1, 'mwei')
+const ONE = BigInt(10) ** BigInt(18)
+
+const tokenZAbi = [
+    {
+        'inputs': [
+            {
+                'internalType': 'address',
+                'name': 'account',
+                'type': 'address'
+            }
+        ],
+        'name': 'balanceOf',
+        'outputs': [
+            {
+                'internalType': 'uint256',
+                'name': '',
+                'type': 'uint256'
+            }
+        ],
+        'stateMutability': 'view',
+        'type': 'function'
+    },
+    {
+        'inputs': [
+            {
+                'internalType': 'address',
+                'name': 'spender',
+                'type': 'address'
+            },
+            {
+                'internalType': 'uint256',
+                'name': 'amount',
+                'type': 'uint256'
+            }
+        ],
+        'name': 'approve',
+        'outputs': [
+            {
+                'internalType': 'bool',
+                'name': '',
+                'type': 'bool'
+            }
+        ],
+        'stateMutability': 'nonpayable',
+        'type': 'function'
+    }
+]
 
 document.getElementById('detectMetaMaskBtn').addEventListener('click', async () => {
     const chainId = await ethereum.request({method: 'eth_chainId'});
-    ethereum.request({ method: 'eth_requestAccounts' })
-
-    const accounts = await web3.eth.getAccounts()
-
     if (chainId !== '0xaa36a7') {
         alert('Only SEPOLIA testnet is supported !')
         return
     }
 
+    await ethereum.request({method: 'eth_requestAccounts'}).catch(console.error)
+    const accounts = await web3.eth.getAccounts()
     if (!accounts || accounts.length === 0) {
         alert('You must allow at least 1 account')
         return
@@ -27,42 +75,22 @@ document.getElementById('detectMetaMaskBtn').addEventListener('click', async () 
 
     initialized = true
 
-    const ERC20BalanceOfAbi = [{
-        constant: true,
-        inputs: [
-            {
-                name: "_owner",
-                type: "address",
-            },
-        ],
-        name: "balanceOf",
-        outputs: [
-            {
-                name: "balance",
-                type: "uint256",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    }]
-
-    const contract = new web3.eth.Contract(ERC20BalanceOfAbi, ZTOKEN_ADDRESS);
+    const tokenZContract = new web3.eth.Contract(tokenZAbi, TOKENZ_ADDRESS);
     document.getElementById("balanceField").innerText = '...'
-    contract.methods
+    tokenZContract.methods
         .balanceOf(account)
         .call()
         .then(balance => document.getElementById("balanceField").innerText = web3.utils.fromWei(balance, "ether"));
 })
 
-document.getElementById('claimZTokenFromFaucetBtn').addEventListener('click', async () => {
+document.getElementById('claimTokenZFromFaucetBtn').addEventListener('click', async () => {
     if (!initialized) {
         alert('You haven\'t initialized MetaMask yet.\nPlease click the "Detect MetaMask" button first')
         return
     }
 
     const account = (await web3.eth.getAccounts())[0]
-    const ZTokenFaucetAbi = [{
+    const tokenZFaucetAbi = [{
         'inputs': [],
         'name': 'request',
         'outputs': [],
@@ -70,34 +98,160 @@ document.getElementById('claimZTokenFromFaucetBtn').addEventListener('click', as
         'type': 'function'
     }]
 
-    const faucet = new web3.eth.Contract(ZTokenFaucetAbi, FAUCET_ADDRESS)
-
+    const faucetContract = new web3.eth.Contract(tokenZFaucetAbi, FAUCET_ADDRESS)
     const requestFunctionSignature = web3.eth.abi.encodeFunctionSignature('request()')
 
-    // get estimated gas, if error use 1e6 as default value
-    const estimatedGas = await web3.eth.estimateGas({
-        from: account,
-        data: requestFunctionSignature,
-        to: FAUCET_ADDRESS
-    }).catch(1e6)
     // get estimated gas price, if error use 1e9 as default value
     const gasPrice = await web3.eth.getGasPrice().catch(1e9)
 
-    console.log(`Starting to claim from address ${account} with:\n  Gas: ${estimatedGas}\n  Gas price: ${gasPrice}`)
-    faucet.methods
+    console.log(`Starting to claim from address ${account} with:\n  Gas price: ${gasPrice}`)
+    faucetContract.methods
         .request()
         .send({
             from: account,
-            gas: estimatedGas.toString(),
+            gas: ONE_MWEI.toString(),
             gasPrice: gasPrice.toString(),
             data: requestFunctionSignature
         })
-        .then(() => alert('Successfully claimed 1000 tokens.'))
+        .then(() => {
+            alert('Successfully claimed 1000 tokens.')
+            document.getElementById('detectMetaMaskBtn').click()
+        })
         .catch((error, receipt) => {
             alert(`ERROR! Check the console for details`)
             console.error(error);
             if (receipt !== undefined) {
                 console.error(receipt)
             }
+        })
+})
+
+document.getElementById('approveGameBtn').addEventListener('click', async () => {
+    if (!initialized) {
+        alert('You haven\'t initialized MetaMask yet.\nPlease click the "Detect MetaMask" button first')
+        return
+    }
+
+    const account = (await web3.eth.getAccounts())[0]
+    const tokenZContract = new web3.eth.Contract(tokenZAbi, TOKENZ_ADDRESS)
+
+    const approveFunctionSignature = web3.eth.abi.encodeFunctionSignature('request(address,uint256)')
+
+
+    // get estimated gas price, if error use 1e9 as default value
+    const gasPrice = await web3.eth.getGasPrice().catch(1e9)
+
+    tokenZContract.methods
+        .approve(account, ONE * BigInt(1000000))
+        .send({
+            from: account,
+            gas: ONE_MWEI.toString(),
+            gasPrice: gasPrice.toString(),
+            data: approveFunctionSignature
+        })
+        .then((ok) => {
+            alert(ok ? 'Successfully approve VRF Game' : 'Failed to approve VRF Game')
+        })
+        .catch((error, receipt) => {
+            alert(`ERROR! Check the console for details`)
+            console.error(error);
+            if (receipt !== undefined) {
+                console.error(receipt)
+            }
+        })
+})
+
+function getOutcome(outcome) {
+    if (outcome === 1n) {
+        return "100% win";
+    } else if (outcome === 2n) {
+        return "20% win";
+    } else if (outcome === 3n) {
+        return "Draw";
+    } else if (outcome === 4n) {
+        return "20% loss";
+    } else if (outcome === 5n) {
+        return "100% loss";
+    }
+}
+
+document.getElementById('playBtn').addEventListener('click', async () => {
+    if (!initialized) {
+        alert('You haven\'t initialized MetaMask yet.\nPlease click the "Detect MetaMask" button first')
+        return
+    }
+
+    const account = (await web3.eth.getAccounts())[0]
+
+    const VRFGameMakeBetAbi = [
+        {
+            'inputs': [
+                {
+                    'internalType': 'uint256',
+                    'name': 'amount',
+                    'type': 'uint256'
+                }
+            ],
+            'name': 'makeBet',
+            'outputs': [
+                {
+                    'internalType': 'uint256',
+                    'name': 'gameId',
+                    'type': 'uint256'
+                }
+            ],
+            'stateMutability': 'nonpayable',
+            'type': 'function'
+        },
+        {
+            'anonymous': false,
+            'inputs': [
+                {
+                    'indexed': true,
+                    'internalType': 'uint256',
+                    'name': 'gameId',
+                    'type': 'uint256'
+                },
+                {
+                    'indexed': true,
+                    'internalType': 'enum VRFGame.Outcomes',
+                    'name': 'outcome',
+                    'type': 'uint8'
+                }
+            ],
+            'name': 'GameFinished',
+            'type': 'event'
+        }
+    ]
+
+    const makeBetContract = new web3.eth.Contract(VRFGameMakeBetAbi, VRFGAME_ADDRESS)
+    const makeBetFunctionSignature = web3.eth.abi.encodeFunctionSignature('makeBet(uint256)')
+
+    // get estimated gas price, if error use 1e9 as default value
+    const gasPrice = await web3.eth.getGasPrice().catch(1e9)
+
+    makeBetContract.methods
+        .makeBet(ONE * BigInt(100))
+        .send({
+            from: account,
+            gas: ONE_MWEI.toString(),
+            gasPrice: gasPrice.toString(),
+            data: makeBetFunctionSignature
+        })
+        .then((receipt) => alert(`Placed one game at tx ${receipt.transactionHash}`))
+        .catch((error, receipt) => {
+            alert(`ERROR! Check the console for details`)
+            console.error(error);
+            if (receipt !== undefined) {
+                console.error(receipt)
+            }
+        })
+
+    makeBetContract.events
+        .GameFinished()
+        .on('data', (event) => {
+            console.log(`Received a GameFinished event`)
+            alert(`Game outcome: ${getOutcome(event.returnValues.outcome)}`)
+            document.getElementById('detectMetaMaskBtn').click()
         })
 })
